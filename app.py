8e6203e1ed0a7e7ab7ed7a4e27f3dba83b6bce91
JECaballeroR <je.caballero10@uniandes.edu.co>
2023-10-14 09:47:24 -0500

    Modificación al host 0.0.0.0 en la instancia.

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ import numpy as np
 import pandas as pd
 import datetime as dt
 
-
-
 app = dash.Dash(
     __name__,
     meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
@@ -23,18 +21,19 @@ app.config.suppress_callback_exceptions = True
 def load_data():
     data = pd.read_csv("datos_energia.csv")
     data['time'] = pd.to_datetime(data['time'])
-    data.index=data['time']
+    data.index = data['time']
     return data
-    # To do: Completar la función 
-    
+    # To do: Completar la función
+
 
 # Cargar datos
 data = load_data()
 
+
 # Graficar serie
 def plot_series(data, initial_date, proy):
     data_plot = data.loc[initial_date:]
-    data_plot = data_plot[:-(120-proy)]
+    data_plot = data_plot[:-(120 - proy)]
     fig = go.Figure([
         go.Scatter(
             name='Demanda energética',
@@ -48,7 +47,7 @@ def plot_series(data, initial_date, proy):
             x=data_plot.index,
             y=data_plot['forecast'],
             mode='lines',
-            line=dict(color="#bbffeb",),
+            line=dict(color="#bbffeb", ),
         ),
         go.Scatter(
             name='Upper Bound',
@@ -81,17 +80,16 @@ def plot_series(data, initial_date, proy):
             x=1
         ),
         yaxis_title='Demanda total [MW]',
-        #title='Continuous, variable value error bars',
+        # title='Continuous, variable value error bars',
         hovermode="x"
     )
-    #fig = px.line(data2, x='local_timestamp', y="Demanda total [MW]", markers=True, labels={"local_timestamp": "Fecha"})
+    # fig = px.line(data2, x='local_timestamp', y="Demanda total [MW]", markers=True, labels={"local_timestamp": "Fecha"})
     fig.update_layout(paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)', font_color="#2cfec1")
     fig.update_xaxes(showgrid=True, gridwidth=0.25, gridcolor='#7C7C7C')
     fig.update_yaxes(showgrid=True, gridwidth=0.25, gridcolor='#7C7C7C')
-    #fig.update_traces(line_color='#2cfec1')
+    # fig.update_traces(line_color='#2cfec1')
 
     return fig
-
 
 
 def description_card():
@@ -101,7 +99,7 @@ def description_card():
     return html.Div(
         id="description-card",
         children=[
-            #html.H5("Proyecto 1"),
+            # html.H5("Proyecto 1"),
             html.H3("Pronóstico de producción energética"),
             html.Div(
                 id="intro",
@@ -133,21 +131,21 @@ def generate_control_card():
                                 min_date_allowed=min(data.index.date),
                                 max_date_allowed=max(data.index.date),
                                 initial_visible_month=min(data.index.date),
-                                date=max(data.index.date)-dt.timedelta(days=7)
+                                date=max(data.index.date) - dt.timedelta(days=7)
                             )
                         ],
                         style=dict(width='30%')
                     ),
-                    
-                    html.P(" ",style=dict(width='5%', textAlign='center')),
-                    
+
+                    html.P(" ", style=dict(width='5%', textAlign='center')),
+
                     html.Div(
                         id="componente-hora",
                         children=[
                             dcc.Dropdown(
                                 id="dropdown-hora-inicial-hora",
-                                options=[{"label": i, "value": i} for i in np.arange(0,25)],
-                                value=pd.to_datetime(max(data.index)-dt.timedelta(days=7)).hour,
+                                options=[{"label": i, "value": i} for i in np.arange(0, 25)],
+                                value=pd.to_datetime(max(data.index) - dt.timedelta(days=7)).hour,
                                 # style=dict(width='50%', display="inline-block")
                             )
                         ],
@@ -174,8 +172,8 @@ def generate_control_card():
                         tooltip={"placement": "bottom", "always_visible": True},
                     )
                 ]
-            )     
-     
+            )
+
         ]
     )
 
@@ -183,25 +181,24 @@ def generate_control_card():
 app.layout = html.Div(
     id="app-container",
     children=[
-        
+
         # Left column
         html.Div(
             id="left-column",
             className="four columns",
             children=[description_card(), generate_control_card()]
-            + [
-                html.Div(
-                    ["initial child"], id="output-clientside", style={"display": "none"}
-                )
-            ],
+                     + [
+                         html.Div(
+                             ["initial child"], id="output-clientside", style={"display": "none"}
+                         )
+                     ],
         ),
-        
+
         # Right column
         html.Div(
             id="right-column",
             className="eight columns",
             children=[
-
 
                 # Grafica de la serie de tiempo
                 html.Div(
@@ -210,12 +207,11 @@ app.layout = html.Div(
                         html.B("Demanda energética total en Austria [MW]"),
                         html.Hr(),
                         dcc.Graph(
-                            id="plot_series",  
+                            id="plot_series",
                         )
                     ],
                 ),
 
-            
             ],
         ),
     ],
@@ -225,11 +221,10 @@ app.layout = html.Div(
 @app.callback(
     Output(component_id="plot_series", component_property="figure"),
     [Input(component_id="datepicker-inicial", component_property="date"),
-    Input(component_id="dropdown-hora-inicial-hora", component_property="value"),
-    Input(component_id="slider-proyeccion", component_property="value")]
+     Input(component_id="dropdown-hora-inicial-hora", component_property="value"),
+     Input(component_id="slider-proyeccion", component_property="value")]
 )
 def update_output_div(date, hour, proy):
-
     if ((date is not None) & (hour is not None) & (proy is not None)):
         hour = str(hour)
         minute = str(0)
@@ -244,4 +239,4 @@ def update_output_div(date, hour, proy):
 
 # Run the server
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(host="0.0.0.0", debug=True)
